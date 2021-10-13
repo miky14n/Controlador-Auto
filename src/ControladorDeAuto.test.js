565bc2b3b32abcff6e4ef7d5497f685346371aab
Jonatan Morales <miky14n@gmail.com>
2021-10-13 17:04:21 -0400

    Retorna 0 cuando se ingresa 0

--- src/ControladorDeAuto.test.js
+++ src/ControladorDeAuto.test.js
@@ -1,13 +1,18 @@
 import {moverAuto} from "./App"
 describe("Controlador de auto", () => {
     it("Test vacio", () => {
+      expect(moverAuto("0")).toEqual("0");
+    });
+    it("Retorna valor por defecto", () => {
       expect(moverAuto()).toEqual("0");
+    });
+    it("Retorna valor por defecto", () => {
+      expect(moverAuto("0")).toEqual("0");
+    });
+    it("Retorna 0 si la cadena esta vacia ", () => {
+      expect(moverAuto("")).toEqual("0");
+    });
     
-  });
-  it("Retorna valor por defecto", () => {
-    expect(moverAuto()).toEqual("0");
-  
-});
   
 }); 
   
